7c403499429b57c12b9596d05d60956d65751103
Misbah602 <mkousar602@gmail.com>
2025-05-01 20:16:07 +0530

    Add files via upload

---
+++ num.php
@@ -0,0 +1,12 @@
+<?php
+// Ceiling value
+$num1 = 443.3432;
+echo "Ceiling value: " . ceil($num1) . "<br>";
+
+// Floor value
+$num2 = 7543.829;
+echo "Floor value: " . floor($num2) . "<br>";
+
+// Random number between 500 and 5000
+echo "Random number: " . rand(500, 5000);
+?>
