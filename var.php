4f7c27d1f7b0b22c9769fd56c1bef73058f48af6
Misbah602 <mkousar602@gmail.com>
2025-05-01 19:56:50 +0530

    Add files via upload

---
+++ var.php
@@ -0,0 +1,19 @@
+<?php
+// Variables
+$collegeName = "Nehru BBA & BCA Collage";
+$regNo = "123456789";
+$branchName = "Bachelor Of Computer Application";
+
+// Print variables
+echo "College Name: " . $collegeName . "<br>";
+echo "Registration Number: " . $regNo . "<br>";
+echo "Branch Name: " . $branchName . "<br>";
+
+// Constants
+define("PHONE_NO", "9876543210");
+define("DEPARTMENT", "Computer Science");
+
+// Print constants
+echo "Phone Number: " . PHONE_NO . "<br>";
+echo "Department: " . DEPARTMENT . "<br>";
+?>
