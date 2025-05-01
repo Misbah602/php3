1a4ccf455b4166fabbd054971f1f044026abf40a
Misbah602 <mkousar602@gmail.com>
2025-05-01 16:45:17 +0530

    Add files via upload

---
+++ p1.php
@@ -0,0 +1,12 @@
+<?php
+// details.php
+
+// Print your name
+echo "My name is Misbah.<br>";
+
+// Print your age
+echo "I am 20 years old.<br>";
+
+// Print your address
+echo "Kamanghar park.<br>";
+?>
